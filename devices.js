3d3a8d6e957c2d0397093ba16879f725d669352a
Koen Kanters <koenkanters94@gmail.com>
2018-04-11 20:54:22 +0200

    Automatically generated docs. #2

--- devices.js
+++ devices.js
@@ -2,18 +2,22 @@ const devices = {
     'lumi.sensor_switch': {
         model: 'WXKG01LM',
         description: 'MiJia wireless switch',
+        supports: 'single, double, triple, quadruple and long click',
     },
     'lumi.sens': {
         model: 'WSDCGQ01LM',
         description: 'MiJia temperature & humidity sensor ',
+        supports: 'temperature and humidity',
     },
     'lumi.sensor_motion': {
         model: 'RTCGQ01LM',
         description: 'MiJia human body movement sensor',
+        supports: 'occupancy, motion and no motion'
     },
     'lumi.sensor_magnet': {
         model: 'MCCGQ01LM',
         description: 'MiJia door & window contact sensor',
+        supports: 'open and closed state',
     },
 }
 
