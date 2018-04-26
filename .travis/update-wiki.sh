0bac4ba9bf49781858949b334250e3348a0d7ab2
Koen Kanters <koenkanters94@gmail.com>
2018-04-26 19:40:44 +0200

    Travis for generating wiki.

---
+++ .travis/update-wiki.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+update_wiki() {
+  rm -rf zigbee2mqtt.wiki
+  git clone https://${GH_TOKEN}@github.com/Koenkk/zigbee2mqtt.wiki.git
+  npm run docgen zigbee2mqtt.wiki
+  cd zigbee2mqtt.wiki
+  git push origin
+}
+
+setup_git
+update_wiki
