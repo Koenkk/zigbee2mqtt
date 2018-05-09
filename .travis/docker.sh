896a7a097c5982558edbda4cc878a09204eba465
Koenkk <koenkanters94@gmail.com>
2018-05-09 22:15:59 +0200

    Use alpine as base image. #22

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -12,10 +12,18 @@ build_and_push() {
 # Only update docker images if on master branch and not pull request
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]
 then
-  echo "Updating docker images!"
+  echo "Updating docker images for master branch!"
   login
-  build_and_push latest docker/Dockerfile
-  build_and_push armv7hf docker/Dockerfile.armv7hf
+  build_and_push latest docker/Dockerfile.amd64
+  build_and_push arm32v6 docker/Dockerfile.arm32v6
+  build_and_push arm64v8 docker/Dockerfile.arm64v8
+elif [ "$TRAVIS_BRANCH" = "dev" -a "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+  echo "Updating docker images for dev branch!"
+  login
+  build_and_push latest-dev docker/Dockerfile.amd64
+  build_and_push arm32v6-dev docker/Dockerfile.arm32v6
+  build_and_push arm64v8-dev docker/Dockerfile.arm64v8
 else
-  echo "Not updating docker images, triggered by pull request or not on master branch"
+  echo "Not updating docker images, triggered by pull request or not on master/dev branch"
 fi
