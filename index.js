03123fb783701c44bc8258ff38c5f24189abda35
Daniel Welch <dwelch2102@gmail.com>
2018-05-11 23:04:15 -0400

    configure datapath with env variable

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const fs = require('fs');
 const Controller = require('./lib/controller');
+const data = require('./lib/util/data');
+
+if (process.env.ZIGBEE2MQTT_DATA) {
+  fs.copyFileSync('./data/configuration.yaml', data.joinPath('configuration.yaml'))
+}
 
 const controller = new Controller();
 controller.start();
