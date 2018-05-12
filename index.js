44002ed1c3d9611aa71a4c47b7dd92275954cdf6
Koenkk <koenkanters94@gmail.com>
2018-05-12 10:58:06 +0200

    Update configure data path code

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const fs = require('fs');
 const Controller = require('./lib/controller');
-const data = require('./lib/util/data');
-
-if (process.env.ZIGBEE2MQTT_DATA) {
-  fs.copyFileSync('./data/configuration.yaml', data.joinPath('configuration.yaml'));
-}
 
 const controller = new Controller();
 controller.start();
