ae41b7bc433810bb3514e21c203b7e8978b5648d
Koenkk <koenkanters94@gmail.com>
2018-07-03 19:17:47 +0200

    Push correct docker tags.

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -21,10 +21,16 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ ! -z ${TRAVIS_TAG+x} ]
 then
   echo "Updating docker images for master branch!"
   login
-  build_and_push latest docker/Dockerfile.amd64
-  tag_push latest "$TRAVIS_TAG"
+
+  # Push versioned images
+  build_and_push "$TRAVIS_TAG" docker/Dockerfile.amd64
   build_and_push "$TRAVIS_TAG-arm32v6" docker/Dockerfile.arm32v6
   build_and_push "$TRAVIS_TAG-arm64v8" docker/Dockerfile.arm64v8
+
+  # Push latest images.
+  tag_push "$TRAVIS_TAG-arm32v6" "arm32v6"
+  tag_push "$TRAVIS_TAG-arm64v8" "arm64v8"
+  tag_push "$TRAVIS_TAG" latest
 elif [ "$TRAVIS_BRANCH" = "dev" -a "$TRAVIS_PULL_REQUEST" = "false" ]
 then
   echo "Updating docker images for dev branch!"
