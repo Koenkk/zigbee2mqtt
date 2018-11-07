fc664bdc6809a6f93c18d75984d4f9cc99d1d5fd
Koenkk <koenkanters94@gmail.com>
2018-11-07 20:05:14 +0100

    Fix node version to >=8.11 10 and implement check on startup. https://github.com/Koenkk/zigbee2mqtt/issues/552

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const Controller = require('./lib/controller');
+const semver = require('semver');
+const engines = require('./package.json').engines;
 
+const version = engines.node;
+if (!semver.satisfies(process.version, version)) {
+    console.log(`\t\tZigbee2mqtt requires node version ${version}, you are running ${process.version}!\n`); // eslint-disable-line
+}
+
+const Controller = require('./lib/controller');
 const controller = new Controller();
 controller.start();
 
