b931c39bbe3024da7ba14a994bc7a02b780a365c
Koenkk <koenkanters94@gmail.com>
2018-11-28 19:34:37 +0100

    Improve tests.

---
+++ test/utils.js
@@ -0,0 +1,13 @@
+const logger = require('../lib/util/logger');
+
+module.exports = {
+    stubLogger: (sandbox) => {
+        sandbox.stub(logger, 'info').callsFake(() => {});
+        sandbox.stub(logger, 'warn').callsFake(() => {});
+        sandbox.stub(logger, 'debug').callsFake(() => {});
+        sandbox.stub(logger, 'error').callsFake(() => {});
+    },
+    zigbeeMessage: (device, cid, type, data) => {
+        return {data: {cid: cid, data: data}, type: type, endpoints: [{device: device}]};
+    },
+};
