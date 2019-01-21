86f19affa16bdb9df328e9984d16a2bc4f1a31c0
Koen Kanters <koenkanters94@gmail.com>
2019-01-21 17:38:06 +0100

    Ensure that state and brightness are executed before other commands. #788

--- test/devicePublish.test.js
+++ test/devicePublish.test.js
@@ -399,4 +399,24 @@ describe('DevicePublish', () => {
         devicePublish.onMQTTMessage('zigbee2mqtt/0x12345678/set', JSON.stringify({state: 1}));
         chai.assert.isTrue(zigbee.publish.notCalled);
     });
+
+    it('Should set state before color', () => {
+        zigbee.publish.resetHistory();
+        zigbee.getDevice = sinon.fake.returns({modelId: 'LCT001'});
+        const msg = {'state': 'ON', 'color': {'x': 0.701, 'y': 0.299}};
+        devicePublish.onMQTTMessage('zigbee2mqtt/0x12345678/set', JSON.stringify(msg));
+        chai.assert.isTrue(zigbee.publish.calledTwice);
+        chai.assert.equal(zigbee.publish.getCall(0).args[2], 'genOnOff');
+        chai.assert.equal(zigbee.publish.getCall(1).args[2], 'lightingColorCtrl');
+    });
+
+    it('Should set state with brightness before color', () => {
+        zigbee.publish.resetHistory();
+        zigbee.getDevice = sinon.fake.returns({modelId: 'LCT001'});
+        const msg = {'state': 'ON', 'color': {'x': 0.701, 'y': 0.299}, 'transition': 3, 'brightness': 100};
+        devicePublish.onMQTTMessage('zigbee2mqtt/0x12345678/set', JSON.stringify(msg));
+        chai.assert.isTrue(zigbee.publish.calledTwice);
+        chai.assert.equal(zigbee.publish.getCall(0).args[2], 'genLevelCtrl');
+        chai.assert.equal(zigbee.publish.getCall(1).args[2], 'lightingColorCtrl');
+    });
 });
