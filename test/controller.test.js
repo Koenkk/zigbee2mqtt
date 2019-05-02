c6617fb7d9af6ac03c603391a74d00cead458ec8
Koen Kanters <koenkanters94@gmail.com>
2019-05-02 20:14:44 +0200

    Always keep state. https://github.com/Koenkk/zigbee2mqtt/issues/1489

--- test/controller.test.js
+++ test/controller.test.js
@@ -69,8 +69,7 @@ describe('Controller', () => {
             controller.onZigbeeMessage(message);
             expect(mqttPublish).toHaveBeenCalledTimes(1);
             expect(JSON.parse(mqttPublish.mock.calls[0][1])).toStrictEqual(payload);
-        }
-        );
+        });
 
         it('Should output to json by default', () => {
             const payload = {temperature: 1, humidity: 2};
@@ -99,6 +98,54 @@ describe('Controller', () => {
             expect(mqttPublish.mock.calls[0][1]).toBe('1');
             expect(mqttPublish.mock.calls[1][0]).toBe('test/humidity');
             expect(mqttPublish.mock.calls[1][1]).toBe('2');
+        });
+
+        it('Should cache state', () => {
+            jest.spyOn(settings, 'get').mockReturnValue({
+                mqtt: {
+                    include_device_information: false,
+                },
+                advanced: {
+                    cache_state: true,
+                },
+                experimental: {
+                    output: 'json',
+                },
+            });
+
+            controller.publishEntityState('0x12345678', {temperature: 1});
+            controller.publishEntityState('0x12345678', {humidity: 2});
+            controller.publishEntityState('0x12345678', {humidity: 5, temperature: 4});
+            controller.publishEntityState('0x12345678', {temperature: 9});
+            controller.publishEntityState('0x12345678', {temperature: 3});
+            expect(mqttPublish).toHaveBeenCalledTimes(5);
+            expect(JSON.parse(mqttPublish.mock.calls[0][1])).toStrictEqual({temperature: 1});
+            expect(JSON.parse(mqttPublish.mock.calls[1][1])).toStrictEqual({temperature: 1, humidity: 2});
+            expect(JSON.parse(mqttPublish.mock.calls[2][1])).toStrictEqual({temperature: 4, humidity: 5});
+            expect(JSON.parse(mqttPublish.mock.calls[3][1])).toStrictEqual({temperature: 9, humidity: 5});
+            expect(JSON.parse(mqttPublish.mock.calls[4][1])).toStrictEqual({temperature: 3, humidity: 5});
+        });
+
+        it('Shouldnt cache state', () => {
+            jest.spyOn(settings, 'get').mockReturnValue({
+                mqtt: {
+                    include_device_information: false,
+                },
+                advanced: {
+                    cache_state: false,
+                },
+                experimental: {
+                    output: 'json',
+                },
+            });
+
+            controller.publishEntityState('0x12345678', {temperature: 1});
+            controller.publishEntityState('0x12345678', {humidity: 2});
+            controller.publishEntityState('0x12345678', {temperature: 3});
+            expect(mqttPublish).toHaveBeenCalledTimes(3);
+            expect(JSON.parse(mqttPublish.mock.calls[0][1])).toStrictEqual({temperature: 1});
+            expect(JSON.parse(mqttPublish.mock.calls[1][1])).toStrictEqual({humidity: 2});
+            expect(JSON.parse(mqttPublish.mock.calls[2][1])).toStrictEqual({temperature: 3});
         });
     });
 });
