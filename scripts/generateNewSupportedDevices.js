d562541178a84128004a45805bc663ac0e7bfc4e
Koen Kanters <koenkanters94@gmail.com>
2019-11-09 20:38:47 +0100

    Shepherd -> herdsman.

--- scripts/generateNewSupportedDevices.js
+++ scripts/generateNewSupportedDevices.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const filename = process.argv[2];
 const text = fs.readFileSync(filename, 'utf8');
-const zigbeeShepherdConverters = require('zigbee-shepherd-converters');
+const zigbeeHerdsmanConverters = require('zigbee-herdsman-converters');
 
 
 for (const line of text.split('\n')) {
-    const model = zigbeeShepherdConverters.devices.find((d) => d.model === line);
+    const model = zigbeeHerdsmanConverters.devices.find((d) => d.model === line);
     console.log(`- \`${line}\` ${model.vendor} ${model.description}`);
 }
