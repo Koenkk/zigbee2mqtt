d51109a30a2a4892765dc8fac581b26a0372a473
Koen Kanters <koenkanters94@gmail.com>
2019-11-29 23:09:01 +0100

    fix tests.

--- test/logger.test.js
+++ test/logger.test.js
@@ -1,18 +1,34 @@
 const tmp = require('tmp');
 const dir = tmp.dirSync();
-const settings = require('../lib/util/settings');
-settings.set(['advanced', 'log_directory'], dir.name + '/%TIMESTAMP%');
-let logger = require('../lib/util/logger.js');
+let settings;
 const fs = require('fs');
 const path = require('path');
+const data = require('./stub/data');
+let stdOutWriteOriginal;
 
-describe('Logger', () => {
+describe('onlythis Logger', () => {
+    beforeEach(async () => {
+        data.writeDefaultConfiguration();
+        jest.resetModules();
+        settings = require('../lib/util/settings');
+        settings.set(['advanced', 'log_directory'], dir.name + '/%TIMESTAMP%');
+        settings._reRead();
+        stdOutWriteOriginal = console._stdout.write;
+        console._stdout.write = () => {};
+    });
+
+    afterEach(async () => {
+        console._stdout.write = stdOutWriteOriginal;
+    });
+
     it('Create log directory', () => {
+        const logger = require('../lib/util/logger.js');
         const dirs = fs.readdirSync(dir.name);
         expect(dirs.length).toBe(1);
     });
 
     it('Should cleanup', () => {
+        const logger = require('../lib/util/logger.js');
         for (let i = 0; i < 20; i++) {
             fs.mkdirSync(path.join(dir.name, `log_${i}`));
         }
@@ -23,6 +39,7 @@ describe('Logger', () => {
     })
 
     it('Should not cleanup when there is no timestamp set', () => {
+        const logger = require('../lib/util/logger.js');
         for (let i = 30; i < 40; i++) {
             fs.mkdirSync(path.join(dir.name, `log_${i}`));
         }
@@ -34,12 +51,45 @@ describe('Logger', () => {
     })
 
     it('Set and get log level', () => {
+        const logger = require('../lib/util/logger.js');
         logger.setLevel('debug');
         expect(logger.getLevel()).toBe('debug');
     });
 
-    it('Disabled logging', () => {
+    it('Logger should be console and file by default', () => {
+        const logger = require('../lib/util/logger.js');
+        const pipes = logger._readableState.pipes;
+        expect(pipes.length).toBe(2);
+        expect(pipes[0].constructor.name).toBe('Console');
+        expect(pipes[0].silent).toBe(false);
+        expect(pipes[1].constructor.name).toBe('File');
+        expect(pipes[1].dirname.startsWith(dir.name)).toBeTruthy();
+    });
+
+    it('Logger can be file only', () => {
+        settings.set(['advanced', 'log_output'], ['file']);
+        const logger = require('../lib/util/logger.js');
+        const pipes = logger._readableState.pipes;
+        expect(pipes.length).toBe(2);
+        expect(pipes[0].constructor.name).toBe('Console');
+        expect(pipes[0].silent).toBe(true);
+        expect(pipes[1].constructor.name).toBe('File');
+        expect(pipes[1].dirname.startsWith(dir.name)).toBeTruthy();
+    });
+
+    it('Logger can be console only', () => {
+        settings.set(['advanced', 'log_output'], ['console']);
+        const logger = require('../lib/util/logger.js');
+        const pipes = logger._readableState.pipes;
+        expect(pipes.constructor.name).toBe('Console');
+        expect(pipes.silent).toBe(false);
+    });
+
+    it('onlythis Logger can be nothing', () => {
         settings.set(['advanced', 'log_output'], []);
-        expect(logger.output).toStrictEqual([]);
+        const logger = require('../lib/util/logger.js');
+        const pipes = logger._readableState.pipes;
+        expect(pipes.constructor.name).toBe('Console');
+        expect(pipes.silent).toBe(true);
     });
 });
