abd6d02bb3b87c4880a4a26e225875cee9ffa485
ruben <ruben.malchow@gmail.com>
2020-04-02 10:47:10 +0200

    generate new network key on first start

--- docker/run.sh
+++ docker/run.sh
@@ -10,7 +10,12 @@ echo "Using '$DATA' as data directory"
 
 if [ ! -f "$DATA/configuration.yaml" ]; then
     echo "Creating configuration file..."
-    cp /app/configuration.yaml "$DATA/configuration.yaml"
+    if [ -z "${NETWORK_KEY}" ]; then
+      echo " - NETWORK_KEY environment variable not set, generating one ... "
+      NETWORK_KEY=$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | od -A n -t u1 | awk '{printf "["} {for(i = 1; i< NF; i++) {printf "%s, ", $i}} {printf "%s]\n", $NF}')
+      echo " - NETWORK_KEY is now: ${NETWORK_KEY}"
+    fi
+    cat /app/configuration.yaml |sed s:"NETWORK_KEY":"${NETWORK_KEY}":g > "$DATA/configuration.yaml"
 fi
 
 exec npm start
