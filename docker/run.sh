75c35959bcf759be8e21d0a3c98edcfa96c00ec5
ruben <ruben.malchow@gmail.com>
2020-04-21 22:29:52 +0200

    no need to generate a key

--- docker/run.sh
+++ docker/run.sh
@@ -10,11 +10,6 @@ echo "Using '$DATA' as data directory"
 
 if [ ! -f "$DATA/configuration.yaml" ]; then
     echo "Creating configuration file..."
-    if [ -z "${NETWORK_KEY}" ]; then
-      echo " - NETWORK_KEY environment variable not set, generating one ... "
-      NETWORK_KEY=$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | od -A n -t u1 | awk '{printf "["} {for(i = 1; i< NF; i++) {printf "%s, ", $i}} {printf "%s]\n", $NF}')
-      echo " - NETWORK_KEY is now: ${NETWORK_KEY}"
-    fi
     cp /app/configuration.yaml.tmpl "$DATA/configuration.yaml"
 fi
 
