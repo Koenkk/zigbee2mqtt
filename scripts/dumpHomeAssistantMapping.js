fa5ac2902f90c6e42d674bfb9cc0cf3be931fe38
Koen Kanters <koenkanters94@gmail.com>
2020-10-12 17:20:27 +0200

    Update dumpHomeAssistantMapping.js

--- scripts/dumpHomeAssistantMapping.js
+++ scripts/dumpHomeAssistantMapping.js
@@ -16,14 +16,20 @@ for (const map of mapping) {
     map[1].sort((a, b) => {
         return JSON.stringify(a).localeCompare(JSON.stringify(b));
     });
+
+    const toDelete = [];
     for (const entry of map[1]) {
         if (entry.type === 'light') {
             if (entry.discovery_payload.brightness === false) delete entry.discovery_payload.brightness;
             if (entry.discovery_payload.color_temp === false) delete entry.discovery_payload.color_temp;
             if (entry.discovery_payload.xy === false) delete entry.discovery_payload.xy;
             if (entry.discovery_payload.hs === false) delete entry.discovery_payload.hs;
+        } else if (entry.object_id === 'linkquality') {
+            toDelete.push(entry);
         }
     }
+
+    toDelete.forEach((d) => map[1].splice(map[1].indexOf(d), 1));
 
     console.log(`${map[0]} - ${stringify(map[1])}`);
 }
