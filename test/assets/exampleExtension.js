3ce5bdf1d41c948d548d9881ea7ee065cdeda756
nur <nur.php@gmail.com>
2021-02-20 18:40:56 +0800

    Add tests

--- test/assets/exampleExtension.js
+++ test/assets/exampleExtension.js
@@ -1,6 +1,7 @@
 class Example {
     constructor(zigbee, mqtt, state, publishEntityState, eventBus) {
         this.mqtt = mqtt;
+        this.mqtt.publish('example/extension', 'call from constructor')
     }
 
     onMQTTConnected() {
