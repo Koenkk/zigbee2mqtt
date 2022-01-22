5dedf930242b2dc30727cc22d9249b850b626b70
Christian Schlegel <christian.schlegel@outlook.com>
2022-01-22 20:36:50 +0100

    add binary build

--- cli.js
+++ cli.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
 const path = require('path');
-process.env['ZIGBEE2MQTT_DATA'] = process.env['ZIGBEE2MQTT_DATA'] || path.join(process.env['HOME'], '.z2m');
+
+if (process.env['ZIGBEE2MQTT_DATA']) {
+	
+} else if (process.env['HOME']) {
+	process.env['ZIGBEE2MQTT_DATA'] = process.env['ZIGBEE2MQTT_DATA'] || path.join(process.env['HOME'], '.z2m');
+} else if (process.cwd()) {
+	process.env['ZIGBEE2MQTT_DATA'] = path.join(process.cwd(), 'data');
+}
+	
+console.log('DataDir ' + process.env['ZIGBEE2MQTT_DATA']);
 require('./index');
