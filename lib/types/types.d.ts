db3730ea44c94e619f0d3a73f430ed46999eca0d
Ben Windsor <benwindsor@gmail.com>
2022-10-04 17:02:35 +0100

    Expose value_step to home assistant if it has been set on a numeric type (#14313)

--- lib/types/types.d.ts
+++ lib/types/types.d.ts
@@ -100,7 +100,7 @@ declare global {
 
         interface DefinitionExpose {
             type: string, name?: string, features?: DefinitionExposeFeature[],
-            endpoint?: string, values?: string[], value_off?: string, value_on?: string,
+            endpoint?: string, values?: string[], value_off?: string, value_on?: string, value_step?: number,
             access: number, property: string, unit?: string,
             value_min?: number, value_max?: number}
 
