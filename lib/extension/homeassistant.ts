81f6cd4e0fba1a90f3e34f0b2111108dd33106ee
Koen Kanters <koenkanters94@gmail.com>
2022-12-05 16:51:02 +0100

    add link

--- lib/extension/homeassistant.ts
+++ lib/extension/homeassistant.ts
@@ -1463,6 +1463,7 @@ export default class HomeAssistant extends Extension {
 
         if (entity.isDevice() && entity.definition?.ota && message.hasOwnProperty('update')) {
             message['update']['installed_version'] = entity.zh.softwareBuildID ?
+                // Prepend with v: https://github.com/Koenkk/zigbee2mqtt/pull/15369
                 `v${entity.zh.softwareBuildID}` : 'unknown';
         }
     }
