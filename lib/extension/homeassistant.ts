1c7817ad778770f05ea8cc1603d37af7045c2209
Koen Kanters <koenkanters94@gmail.com>
2023-01-02 09:18:36 +0100

    Fix Home Assistant `Template variable warning: 'dict object' has no attribute 'latest_version' when rendering '{{ value_json['update']['latest_version'] }}'` error. https://github.com/Koenkk/zigbee2mqtt/issues/15747

--- lib/extension/homeassistant.ts
+++ lib/extension/homeassistant.ts
@@ -1478,7 +1478,7 @@ export default class HomeAssistant extends Extension {
             }
         }
 
-        if (entity.isDevice() && entity.definition?.ota && !message.update?.hasOwnProperty('installed_version')) {
+        if (entity.isDevice() && entity.definition?.ota && !message.update?.hasOwnProperty('latest_version')) {
             message.update = {...message.update, installed_version: -1, latest_version: -1};
         }
     }
