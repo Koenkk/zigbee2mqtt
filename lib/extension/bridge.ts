1df5fc26a9bc020d3589882a3384c2bfea441378
Koen Kanters <koenkanters94@gmail.com>
2024-07-11 22:09:10 +0200

    updates

--- lib/extension/bridge.ts
+++ lib/extension/bridge.ts
@@ -536,14 +536,14 @@ export default class Bridge extends Extension {
         try {
             await device.zh.interview(true);
             logger.info(`Successfully interviewed '${device.name}'`);
-            // A re-interview can for example result in a different modelId, therefore reconsider the definition.
-            await device.resolveDefinition(true);
         } catch (error) {
             throw new Error(`interview of '${device.name}' (${device.ieeeAddr}) failed: ${error}`, {cause: error});
         }
 
-        // publish devices so that the front-end has up-to-date info.
-        await this.publishDevices();
+        // A re-interview can for example result in a different modelId, therefore reconsider the definition.
+        await device.resolveDefinition(true);
+        this.eventBus.emitDevicesChanged();
+        this.eventBus.emitExposesChanged({device});
 
         return utils.getResponse(message, {id: message.id}, null);
     }
