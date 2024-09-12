1c88f022eefeec3a7be2e4815c264781507374dd
Tomer <57483589+tomer-w@users.noreply.github.com>
2024-09-12 07:44:53 +0300

    Run prettier

--- lib/zigbee.ts
+++ lib/zigbee.ts
@@ -56,7 +56,10 @@ export default class Zigbee {
             acceptJoiningDeviceHandler: this.acceptJoiningDeviceHandler,
         };
 
-        logger.debug(() => `Using zigbee-herdsman with settings: '${stringify(JSON.stringify(herdsmanSettings).replaceAll(JSON.stringify(herdsmanSettings.network.networkKey), '"HIDDEN"'))}'`);
+        logger.debug(
+            () =>
+                `Using zigbee-herdsman with settings: '${stringify(JSON.stringify(herdsmanSettings).replaceAll(JSON.stringify(herdsmanSettings.network.networkKey), '"HIDDEN"'))}'`,
+        );
 
         let startResult;
         try {
@@ -111,8 +114,9 @@ export default class Zigbee {
         this.herdsman.on('message', async (data: ZHEvents.MessagePayload) => {
             const device = this.resolveDevice(data.device.ieeeAddr)!;
             await device.resolveDefinition();
-            logger.debug(() =>
-                `Received Zigbee message from '${device.name}', type '${data.type}', ` +
+            logger.debug(
+                () =>
+                    `Received Zigbee message from '${device.name}', type '${data.type}', ` +
                     `cluster '${data.cluster}', data '${stringify(data.data)}' from endpoint ${data.endpoint.ID}` +
                     (data.hasOwnProperty('groupID') ? ` with groupID ${data.groupID}` : ``) +
                     (device.zh.type === 'Coordinator' ? `, ignoring since it is from coordinator` : ``),
