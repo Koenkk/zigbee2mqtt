11277ef2dba86b67376061d3a69e23536faad674
Koen Kanters <koenkanters94@gmail.com>
2024-10-03 20:56:28 +0200

    Update link

--- lib/controller.ts
+++ lib/controller.ts
@@ -167,7 +167,7 @@ export class Controller {
             logger.error((error as Error).stack!);
             /* istanbul ignore if */
             if (error instanceof Error && error.message.includes('USB adapter discovery error (No valid USB adapter found)')) {
-                logger.error('If this happens after updating to Zigbee2MQTT 2.0.0, see https://github.com/Koenkk/zigbee2mqtt/issues/24187');
+                logger.error('If this happens after updating to Zigbee2MQTT 2.0.0, see https://github.com/Koenkk/zigbee2mqtt/discussions/24199');
             }
             return await this.exit(1);
         }
