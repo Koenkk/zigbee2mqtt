aa75264425ea03fa4f115ef90a077e278dc5aa45
XS400DOHC <63352197+xs400dohc@users.noreply.github.com>
2025-02-12 22:51:08 +0100

    possible solution for issue #26028

--- lib/extension/homeassistant.ts
+++ lib/extension/homeassistant.ts
@@ -1023,7 +1023,7 @@ export default class HomeAssistant extends Extension {
                 else if (firstExpose.unit && ['A', 'mA'].includes(firstExpose.unit)) {
                     Object.assign(extraAttrs, {device_class: 'current', state_class: 'measurement'});
                 }
-                // If a variable includes mW, W, kW, MW, GW, TW, mark it as power
+                // If a variable includes mW, W, kW mark it as power
                 else if (firstExpose.unit && ['mW', 'W', 'kW'].includes(firstExpose.unit)) {
                     Object.assign(extraAttrs, {device_class: 'power', state_class: 'measurement'});
                 }
