2f0883d267ab624c98e5c343116f9c4e0b0a1467
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-02-18 15:48:38 +0100

    fix: Sync new definition API from ZHC (#26403)
    Co-authored-by: Koenkk <2892853+Koenkk@users.noreply.github.com>
    Co-authored-by: Koen Kanters <koenkanters94@gmail.com>

--- test/extensions/onEvent.test.ts
+++ test/extensions/onEvent.test.ts
@@ -11,8 +11,6 @@ import * as settings from '../../lib/util/settings';
 
 const mockOnEvent = vi.fn();
 const mockLivoloOnEvent = vi.fn();
-const mappedLivolo = zhc.findByModel('TI0001')!;
-mappedLivolo.onEvent = mockLivoloOnEvent;
 // @ts-expect-error mock
 zhc.onEvent = mockOnEvent;
 
@@ -20,6 +18,11 @@ const mocksClear = [mockMQTTPublishAsync, mockLogger.warning, mockLogger.debug];
 
 describe('Extension: OnEvent', () => {
     let controller: Controller;
+
+    beforeAll(async () => {
+        const mappedLivolo = (await zhc.findByDevice(devices.LIVOLO))!;
+        mappedLivolo.onEvent = mockLivoloOnEvent;
+    });
 
     beforeEach(async () => {
         vi.useFakeTimers();
