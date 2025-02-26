ba14e1c755bd21b2498f80bbc2515f6ab5839230
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-02-26 22:07:37 +0100

    feat: Use dynamic import for external JS

--- lib/extension/externalConverters.ts
+++ lib/extension/externalConverters.ts
@@ -1,13 +1,13 @@
-import type * as zhc from 'zigbee-herdsman-converters';
+import type {ExternalDefinitionWithExtend} from 'zigbee-herdsman-converters';
 
 import {addExternalDefinition, removeExternalDefinitions} from 'zigbee-herdsman-converters';
 
 import logger from '../util/logger';
 import ExternalJSExtension from './externalJS';
 
-type ModuleExports = zhc.ExternalDefinitionWithExtend | zhc.ExternalDefinitionWithExtend[];
+type TModule = ExternalDefinitionWithExtend | ExternalDefinitionWithExtend[];
 
-export default class ExternalConverters extends ExternalJSExtension<ModuleExports> {
+export default class ExternalConverters extends ExternalJSExtension<TModule> {
     constructor(
         zigbee: Zigbee,
         mqtt: MQTT,
@@ -33,27 +33,29 @@ export default class ExternalConverters extends ExternalJSExtension<ModuleExport
     }
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    protected async removeJS(name: string, module: ModuleExports): Promise<void> {
+    protected async removeJS(name: string, mod: TModule): Promise<void> {
         removeExternalDefinitions(name);
 
         await this.zigbee.resolveDevicesDefinitions(true);
     }
 
-    protected async loadJS(name: string, module: ModuleExports): Promise<void> {
+    protected async loadJS(name: string, mod: TModule, newName?: string): Promise<void> {
         try {
             removeExternalDefinitions(name);
 
-            const definitions = Array.isArray(module) ? module : [module];
+            const definitions = Array.isArray(mod) ? mod : [mod];
+
             for (const definition of definitions) {
-                definition.externalConverterName = name;
+                definition.externalConverterName = newName ?? name;
 
                 addExternalDefinition(definition);
-                logger.info(`Loaded external converter '${name}'.`);
+                logger.info(`Loaded external converter '${newName ?? name}'.`);
             }
 
             await this.zigbee.resolveDevicesDefinitions(true);
         } catch (error) {
-            logger.error(`Failed to load external converter '${name}'`);
+            /* v8 ignore next */
+            logger.error(`Failed to load external converter '${newName ?? name}'`);
             logger.error(`Check the code for syntax error and make sure it is up to date with the current Zigbee2MQTT version.`);
             logger.error(
                 `External converters are not meant for long term usage, but for local testing after which a pull request should be created to add out-of-the-box support for the device`,
--- test/assets/external_converters/mjs/mock-external-converter.mjs
+++ test/assets/external_converters/mjs/mock-external-converter.mjs
@@ -1,12 +1,12 @@
-const mockDevice = {
+import {posix} from 'node:path';
+
+export default {
     mock: true,
     zigbeeModel: ['external_converter_device'],
     vendor: 'external',
     model: 'external_converter_device',
-    description: 'external',
+    description: posix.join('external', 'converter'),
     fromZigbee: [],
     toZigbee: [],
     exposes: [],
 };
-
-module.exports = mockDevice;
