1b97b455dc2641b9595959a7c42b3dc4128b853c
Nerivec <62446222+Nerivec@users.noreply.github.com>
2025-02-26 22:07:37 +0100

    feat: Use dynamic import for external JS

---
+++ test/assets/external_converters/cjs/mock-external-converter.js
@@ -0,0 +1,14 @@
+const {posix} = require('node:path');
+
+const mockDevice = {
+    mock: true,
+    zigbeeModel: ['external_converter_device'],
+    vendor: 'external',
+    model: 'external_converter_device',
+    description: posix.join('external', 'converter'),
+    fromZigbee: [],
+    toZigbee: [],
+    exposes: [],
+};
+
+module.exports = mockDevice;
