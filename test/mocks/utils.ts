06ba62b8331d8d5db1696c47bd3b643449584b16
Koen Kanters <koenkanters94@gmail.com>
2025-03-11 21:55:32 +0100

    fix(ignore): update zigbee-herdsman-converters to 23.6.0 (#26691)

--- test/mocks/utils.ts
+++ test/mocks/utils.ts
@@ -1,3 +1,5 @@
+import type {DefinitionWithExtend} from 'zigbee-herdsman-converters';
+
 export type EventHandler = (...args: unknown[]) => unknown;
 
 export async function flushPromises(): Promise<void> {
@@ -11,4 +13,8 @@ export function defuseRejection<T>(promise: Promise<T>): Promise<T> {
     promise.catch(() => {});
 
     return promise;
+}
+
+export async function getZhcBaseDefinitions(): Promise<DefinitionWithExtend[]> {
+    return (await import('zigbee-herdsman-converters/devices/index')).default;
 }
