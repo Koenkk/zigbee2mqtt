e2b7df9f84550aa413f3453bb97a13ba2eee4187
Koen Kanters <koenkanters94@gmail.com>
2025-07-15 13:56:17 +0200

    fix(ignore): Remove retain/qos from mqtt publish logging https://github.com/Koenkk/zigbee2mqtt/pull/27983

--- lib/mqtt.ts
+++ lib/mqtt.ts
@@ -231,11 +231,7 @@ export default class Mqtt {
         }
 
         if (!finalOptions.skipLog) {
-            logger.info(
-                () =>
-                    `MQTT publish: topic '${topic}', payload '${payload}' [retain: ${clientOptions.retain ?? false}, qos: ${clientOptions.qos ?? 0}]`,
-                NS,
-            );
+            logger.info(() => `MQTT publish: topic '${topic}', payload '${payload}'`, NS);
         }
 
         try {
