f33833417d6f53b514fb245ba7a930d2d382caa1
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2025-11-26 21:38:18 +0000

    Add `currentHumidity` to `climate`

--- lib/extension/homeassistant.ts
+++ lib/extension/homeassistant.ts
@@ -752,6 +752,14 @@ export class HomeAssistant extends Extension {
                     discoveryEntries.push(discoveryEntry);
                 }
 
+                const currentHumidity = allExposes
+                    ?.filter(isNumericExpose)
+                    .find((e) => ["humidity"].includes(e.name) && e.access === ACCESS_STATE);
+                if (currentHumidity) {
+                    discoveryEntry.discovery_payload.current_humidity_template = `{{ value_json.${currentHumidity.property} }}`;
+                    discoveryEntry.discovery_payload.current_humidity_topic = true;
+                }
+
                 discoveryEntries.push(discoveryEntry);
                 break;
             }
