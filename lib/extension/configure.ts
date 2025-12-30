4d6e98a434b567b1e5c38defec46bb110368f4be
Koen Kanters <koenkanters94@gmail.com>
2025-12-30 21:55:03 +0100

    fix(ignore): Changes for new ZHC `configureKey`

--- lib/extension/configure.ts
+++ lib/extension/configure.ts
@@ -93,14 +93,20 @@ export default class Configure extends Extension {
             return;
         }
 
+        const configureKey = zhc.getConfigureKey(device.definition);
         if (!force) {
             if (device.options.disabled || !device.interviewed) {
                 return;
             }
 
-            if (device.zh.meta?.configured !== undefined) {
-                return;
-            }
+            const shouldReconfigure =
+                // Should always reconfigure when not configured before
+                device.zh.meta?.configured === undefined ||
+                // Or should reconfigure when configureKey is not the default one (0) and differs from last configured configureKey
+                // In older z2m versions the stored configureKey (meta.configured) was the hash of the configure function.
+                // Since we don't want to reconfigure all devices, we don't re-configure when a device has the default configureKey of 0.
+                (configureKey !== 0 && device.zh.meta?.configured !== configureKey);
+            if (!shouldReconfigure) return;
 
             // Only configure end devices when it is active, otherwise it will likely fails as they are sleeping.
             if (device.zh.type === "EndDevice" && event !== "zigbee_event") {
@@ -122,7 +128,7 @@ export default class Configure extends Extension {
         try {
             await device.definition.configure(device.zh, this.zigbee.firstCoordinatorEndpoint(), device.definition);
             logger.info(`Successfully configured '${device.name}'`);
-            device.zh.meta.configured = zhc.getConfigureKey(device.definition);
+            device.zh.meta.configured = configureKey;
             device.zh.save();
             this.eventBus.emitDevicesChanged();
         } catch (error) {
