428e93f980e1e986a2f898b04b28fc07c20ef885
Kai Tanaka <275430420+quyentonndbs@users.noreply.github.com>
2026-06-09 19:25:38 +0000

    chore: improve zigbee2mqtt maintenance path

--- test/data.test.ts
+++ test/data.test.ts
@@ -15,12 +15,21 @@ describe("Data", () => {
             const expected = tmp.dirSync().name;
             process.env.ZIGBEE2MQTT_DATA = expected;
             data._testReload();
-            const actual = data.getPath();
-            expect(actual).toBe(expected);
-            expect(data.joinPath("test")).toStrictEqual(path.join(expected, "test"));
-            expect(data.joinPath("/test")).toStrictEqual(path.resolve(expected, "/test"));
-            delete process.env.ZIGBEE2MQTT_DATA;
-            data._testReload();
+
+            try {
+                const actual = data.getPath();
+                expect(actual).toBe(expected);
+                expect(data.joinPath("test")).toStrictEqual(path.join(expected, "test"));
+                expect(data.joinPath("/test")).toStrictEqual(path.resolve(expected, "/test"));
+            } finally {
+                delete process.env.ZIGBEE2MQTT_DATA;
+                data._testReload();
+            }
+        });
+
+        it("Should return data path when joinPath with empty string", () => {
+            const expected = data.getPath();
+            expect(data.joinPath("")).toStrictEqual(expected);
         });
     });
 });
